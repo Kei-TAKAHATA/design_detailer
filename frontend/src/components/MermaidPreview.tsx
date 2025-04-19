5618e00c81a2a62eddd966283477e3eb22ceb730
Kei-TAKAHATA <keitakahataresearch@gmail.com>
2025-04-19 14:20:13 +0900

    [feature]箱作成後の保存

---
+++ frontend/src/components/MermaidPreview.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MermaidPreview: React.FC = () => {
+  return (
+    <div>
+      <h2>Mermaid Diagram Preview</h2>
+      <div id="mermaid-diagram"></div>
+    </div>
+  );
+};
+
+export default MermaidPreview;
