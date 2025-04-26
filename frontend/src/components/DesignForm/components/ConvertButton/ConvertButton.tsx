8f844a037dcf48ad301723247d15a11eb1cc76e1
Kei-TAKAHATA <keitakahataresearch@gmail.com>
2025-04-26 18:31:22 +0900

    [delete]ボタンの共通コンポーネント作成後の変換ボタン削除

--- frontend/src/components/DesignForm/components/ConvertButton/ConvertButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyledConvertButton } from './styles';
-import { ConvertButtonProps } from './types';
-
-export const ConvertButton: React.FC<ConvertButtonProps> = ({ onClick }) => {
-  return (
-    <StyledConvertButton onClick={onClick}>変換</StyledConvertButton>
-  );
-};
