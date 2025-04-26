2d2f94467b2b01db0e86e14158eb4f62efa56f23
Kei-TAKAHATA <184033287+Kei-TAKAHATA@users.noreply.github.com>
2025-04-26 19:21:04 +0900

    [update]コードブロックのデザイン変更

--- frontend/src/components/MermaidEditor/MermaidEditor.tsx
+++ frontend/src/components/MermaidEditor/MermaidEditor.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { MermaidEditorProps } from './types';
-import { StyledMermaidEditor } from './styles';
-import { TextArea } from '../common/TextArea';
+import { StyledMermaidEditor, StyledMermaidEditorTextArea } from './styles';
+
 
 export const MermaidEditor: React.FC<MermaidEditorProps> = ({ code, onChange }) => {
   const [lineCount, setLineCount] = useState(code.split('\n').length);
@@ -12,14 +12,13 @@ export const MermaidEditor: React.FC<MermaidEditorProps> = ({ code, onChange }) 
 
   return (
     <StyledMermaidEditor>
-      <TextArea
+      <StyledMermaidEditorTextArea
         value={code}
         onChange={(event) => onChange(event.target.value)}
         $minRows={Math.max(lineCount + 1, 5)}
         $maxRows={20}
         $cols={50}
         placeholder="Mermaidのコードを入力してください"
-        // style={{ width: '100%', marginBottom: '20px' }}
       />
     </StyledMermaidEditor>
   );
