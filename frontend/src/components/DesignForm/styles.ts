f39cfebf45de417dea1b7afbe8a4f8775353bd76
Kei-TAKAHATA <keitakahataresearch@gmail.com>
2025-04-26 18:32:22 +0900

    [update]設計概要入力部分のスタイルの更新

--- frontend/src/components/DesignForm/styles.ts
+++ frontend/src/components/DesignForm/styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Button } from "../common/Button";
+
 
 export const StyledDesignForm = styled.div<{ height?: string }>`
   display: flex;
@@ -14,4 +16,20 @@ export const StyledDesignForm = styled.div<{ height?: string }>`
   margin-bottom: 2%; /* 親要素の幅に対する割合で設定 */
   margin-left: 0;  /* 左側のマージンを0に設定 */
   margin-right: 0; /* 右側のマージンを0に設定 */
+`;
+
+// export const StyledDesignFormTextArea = styled(TextArea)`
+//   background: #444;         // コードブロック風のグレー背景色
+//   border-radius: 6px;      // 角丸
+//   padding: 1rem;               // 内側余白
+//   font-family: 'Fira Mono', 'Consolas', 'Menlo', monospace; // 等幅フォント
+//   color: #fff;                 // 文字色
+// `;
+
+
+export const StyledConvertButton = styled(Button)`
+  height: 100%; /* 親要素の高さに合わせる */
+  width: 20%;
+  padding: 0.5rem 4rem; /* ボタンの内側の余白を設定 */
+  margin-left: auto; /* 右揃えにするための設定 */
 `;
